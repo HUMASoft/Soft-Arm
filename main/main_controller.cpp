b235347bacca753f00f178cff96ec23027f77318
Humasoft <humasoft@humasoft.ing.uc3m.es>
2021-07-01 13:40:24 +0200

    Set 3 data

--- main/main_controller.cpp
+++ main/main_controller.cpp
@@ -10,15 +10,19 @@
 //
 #include "imu3dmgx510.h"
 
-const char *PORT = "/dev/ttyUSB0";
+//const char *PORT = "/dev/ttyUSB0";
 //
 
 
 int main ()
 {
 
+    vector<double> ang(2);
+    ang[0] = -90; //ALPHA
+    ang[1] = 0; //BETA
 
-    ofstream data("/home/humasoft/code/Soft-Arm/graphs/Test3_Control_P0_Y-20.csv",std::ofstream::out); // /home/humasoft/code/graficas
+
+    ofstream data("/home/humasoft/code/Soft-Arm/graphs/Test3_Control_P"+to_string(int(ang[0]))+"_Y"+to_string(int(ang[1]))+".csv",std::ofstream::out); // /home/humasoft/code/graficas
     //--Can port communications--
     SocketCanPort pm1("can1");
     CiA402SetupData sd1(2048,157,0.001, 1.25, 20 );
@@ -40,7 +44,7 @@ int main ()
     double radio=0.0093;
 
     vector<double> v_lengths(3);
-    vector<double> ang(2);
+
     double posan1, posan2, posan3;
 
     // SENSOR
@@ -54,16 +58,20 @@ int main ()
     SamplingTime Ts;
     Ts.SetSamplingTime(dts);
 
-
+    //plot
+    IPlot probe(dts);
+    IPlot probe2(dts);
     //Controller
 
-    //FPDBlock conP(0.4506,0.5478,-1.11,dts); //(kp,kd,exp,dts) 0.0214437
-    FPDBlock conP(0.5811,0.5178,-0.97,dts); //(kp,kd,exp,dts) 0.0214437
+    //FPDBlock conP(0.4506,0.5478,-1.11,dts); //(kp,kd,exp,dts) 0.0214437 90 0.5
+    //FPDBlock conP(0.7996,0.8271,-1.17,dts); //80 0.8
+    FPDBlock conP(0.5811,0.5178,-0.97,dts); //(kp,kd,exp,dts) 0.0214437 100 0.5
 
     FPDBlock resetP(conP); //Used for control reset
 
-    //FPDBlock conY(0.6426,0.576,-1.11,dts); //(kp,kd,exp,dts) 0.0214437
-    FPDBlock conY(0.8508,0.4978,-1.02,dts); //(kp,kd,exp,dts) 0.0214437
+    //FPDBlock conY(0.6426,0.576,-1.11,dts); //(kp,kd,exp,dts) 0.0214437 90 0.5
+    //FPDBlock conY(1.232,0.8582,-1.24,dts); //80 0.8
+    FPDBlock conY(0.8508,0.4978,-1.02,dts); //(kp,kd,exp,dts) 0.0214437 100 0.5
 
     FPDBlock resetY(conY); //Used for control reset
     vector<double> ierror(2);
@@ -75,40 +83,30 @@ int main ()
 
     for (double t=0;t<10;t+=dts)
     {
-
-
         misensor.GetPitchRollYaw(pitch,roll,yaw);
-        cout<<"Calibrando"<<endl;
-        cout << "Roll: " << roll*180/M_PI << " Pitch: " <<pitch*180/M_PI  << " Yaw: " << yaw*180/M_PI << endl;
+        //cout<<"Calibrando"<<endl;
+        //cout << "Roll: " << roll*180/M_PI << " Pitch: " <<pitch*180/M_PI  << " Yaw: " << yaw*180/M_PI << endl;
     }
+    cout<<"Calibrado"<<endl;
+    cout<<"Moving to Pitch: "+to_string(int(ang[0]))+ " and Yaw: "+to_string(int(ang[1]))<<endl;
 
-
-
-    ang[0] = 0; //ALPHA
-    ang[1] = -20; //BETA
-
-    //cs[1]=0;
-
-
-
-        double interval=10; //in seconds
+        double interval=20; //in seconds
         for (double t=0;t<interval; t+=dts)
         {
             misensor.GetPitchRollYaw(pitch,roll,yaw);
 
-            //negative feedback
             ierror[0] = ang[0] - pitch*180/M_PI;
-            //cout << "error 1:  " <<ierror[0]<<endl;
-
             ierror[1] = ang[1] - yaw*180/M_PI;
-            cout << "error 2:  " <<ierror[1]<<endl;
+
             //ierror= ierror*M_PI/180; //degrees to rad
+
+            //PLOT DE DATOS
+            //probe.pushBack(pitch*180/M_PI);
+            //probe2.pushBack(yaw*180/M_PI);
 
             //controller computes control signal
             cs[0] = ierror[0] > conP;
             cs[1] = ierror[1] > conY;
-
-            //  cs = ierror > intcon;
 
             if (!isnormal(cs[0])) cs[0] = 0;
 
@@ -130,21 +128,16 @@ int main ()
             m2.SetPosition(posan2);
             m3.SetPosition(posan3);
 
-            //cout << "Alpha:  " << ang[0] << ", Beta:  " << ang[1] << endl;
-            //cout << "Length variation" << endl;
-            //cout << "1: " << v_lengths[0]  << "2: " << v_lengths[1] << "3: " << v_lengths[2] <<endl;
-            // Sensor data
-            //misensor.GetPitchRollYaw(pitch,roll,yaw);
             data <<ang[0] << " , " <<ang[1]<< " , " << roll << " , " << pitch << " , " << yaw<<" , " <<  m1.GetPosition() <<" , " <<m2.GetPosition() <<" , " <<m3.GetPosition() << " , " << cs[0] << " , " <<cs[1] << endl; //CR
-            //cout << "Roll: " << roll*180/M_PI << " Pitch: " << pitch*180/M_PI << " Yaw: " << yaw*180/M_PI<< endl; //CR
             //cout << endl;
         }
-
+        cout <<"Done" << endl;
         conP = FPDBlock(resetP); //Reset?
         conY = FPDBlock(resetY); //Reset?
 
+        //probe.Plot();
+        //probe2.Plot();
 
-        //ang[0]+=25;
         m1.SetPosition(0);
         m2.SetPosition(0);
         m3.SetPosition(0);
