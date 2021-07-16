86a1bbe8c3eed97c637432130ef6d2046f95f24e
Humasoft <humasoft@humasoft.ing.uc3m.es>
2021-07-16 14:13:43 +0200

    nuevo archivo:  graphs/Test6_Control_10_Y0.csv 	nuevo archivo:  graphs/Test6_Control_20_Y0.csv 	modificado:     lib/fcontrol 	modificado:     lib/sensor-integration 	modificado:     main/main_controller.cpp

--- main/main_controller.cpp
+++ main/main_controller.cpp
@@ -16,7 +16,7 @@ int main ()
 
     vector<double> ang(2);
     ang[0] = 10; //ALPHA
-    ang[1] = 20; //BETA
+    ang[1] = 0; //BETA
 
 
     ofstream data("/home/humasoft/code/Soft-Arm/graphs/Test6_Control_"+to_string(int(ang[0]))+"_Y"+to_string(int(ang[1]))+".csv",std::ofstream::out); // /home/humasoft/code/graficas
@@ -24,17 +24,17 @@ int main ()
     SocketCanPort pm1("can1");
     CiA402SetupData sd1(2048,157,0.001, 1.25, 20 );
     CiA402Device m1 (31, &pm1, &sd1);
-    m1.SetupPositionMode(10,10);
+    m1.SetupPositionMode(3,3);
 
     SocketCanPort pm2("can1");
     CiA402SetupData sd2(2048,157,0.001, 1.25, 20 );
     CiA402Device m2 (32, &pm2, &sd2);    //--Can port communications--
-    m2.SetupPositionMode(10,10);
+    m2.SetupPositionMode(3,3);
 
     SocketCanPort pm3("can1");
     CiA402SetupData sd3(2048,157,0.001, 1.25, 20 );
     CiA402Device m3 (33, &pm3, &sd3);
-    m3.SetupPositionMode(10,10);
+    m3.SetupPositionMode(3,3);
 
     double radio=0.0093;
     vector<double> v_lengths(3);
@@ -48,6 +48,18 @@ int main ()
     double dts=1/freq;
     SamplingTime Ts;
     Ts.SetSamplingTime(dts);
+
+//    vector<double> num{0.000438,0.0004682,0};
+//    vector<double> den{0.8187,-1.817,1};
+    vector<double> num{0.000438*180/M_PI,0.0004682*180/M_PI,0};
+    vector<double> den{0.8187,-1.817,1};
+//    vector<double> num{0.675716,-0.581882,0};
+//    vector<double> den{0.0263503,-0.905029,1};
+    SystemBlock pitchModel(num,den);
+    double pitchPred;
+
+    //identification
+    OnlineSystemIdentification pitchOnlineModel(1,2);
 
     //plot
     IPlot probe(dts);
@@ -89,7 +101,7 @@ int main ()
     cout<<"Calibrado"<<endl;
     cout<<"Moving to Pitch: "+to_string(int(ang[0]))+ " and Yaw: "+to_string(int(ang[1]))<<endl;
 
-    double interval=30; //in seconds
+    double interval=5; //in seconds
     for (double t=0;t<interval; t+=dts)
     {
         misensor.GetPitchRollYaw(pitch,roll,yaw);
@@ -100,25 +112,46 @@ int main ()
         //ierror= ierror*M_PI/180; //degrees to rad
 
         //PLOT DE DATOS
-        probe.pushBack(yaw*180/M_PI);
-        probe1.pushBack(ierror[1]);
+        probe.pushBack(pitch*180/M_PI);
         //probe2.pushBack(yaw*180/M_PI);
+
+        //identification
+        pitchOnlineModel.UpdateSystem(cs[0],pitch*180/M_PI);
+        pitchOnlineModel.PrintZTransferFunction(dts);
 
         //controller computes control signal FPD
 //        cs[0] = ierror[0] > conP;
 //        cs[1] = ierror[1] > conY;
 
         //controller computes control signal PID
-        cs[0] = ierror[0] > conPPID;
+//        cs[0] = ierror[0] > conPPID;
+        if (abs(m1.GetVelocity()) > 2.9 | abs(m2.GetVelocity()) > 2.9 | abs(m3.GetVelocity()) > 2.9)
+        {
+            cs[0] = conPPID.UpdateControlILock(ierror[0]);
+            cout << "m1: " << abs(m1.GetVelocity())<<  ", m2: " << abs(m2.GetVelocity())<<  ", m3: " << abs(m3.GetVelocity()) << endl;
+            cout << cs[0] << endl;
+        }
+        else
+        {
+            cs[0] = ierror[0] > conPPID;
+//            cout << cs[0] << endl;
+        }
+
         cs[1] = ierror[1] > conYPID;
         //cs[0]=0;
 
-        probe2.pushBack(cs[1]);
+        probe2.pushBack(cs[0]);
         //cout << endl;
 
         if (!isnormal(cs[0])) cs[0] = 0;
 
         if (!isnormal(cs[1])) cs[1] = 0;
+
+        pitchPred = 20 > pitchModel;
+        probe1.pushBack(pitchPred);
+
+
+        cs[0] = 20;
 
         v_lengths[0]=0.001*( cs[0] / 1.5);
         //v_lengths[1]=0.001*( (cs[1] / 1.732) - (cs[0] / 3) );
